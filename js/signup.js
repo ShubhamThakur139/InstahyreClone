ca07dcf3aaa56c52d4e98b974325882dfecf4edf
Shubham Thakur <shubhamthakur2920@gmail.com>
2021-08-26 23:48:01 +0530

    js functionality

--- js/signup.js
+++ js/signup.js
@@ -56,10 +56,43 @@ function add_user(e) {
 
   alert("User SignUp Successfully");
   location.href = "../assets/all_details.html";
+}
 
-  function add_details(e) {
-    e.preventDefault();
-    let working = document.getElementsByName("working").id;
-    console.log(working);
-  }
+function add_details(e) {
+  e.preventDefault();
+
+  console.log(--id);
+
+  let work = document.querySelector('input[name="working"]:checked').value;
+  console.log("work:", work);
+  let exp_year = document.getElementById("exp").value;
+  console.log("exp_year:", exp_year);
+  let role = document.getElementById("job_role");
+  let job_rule = role.options[role.selectedIndex].text;
+  console.log("job_rule:", job_rule);
+  let city = document.getElementById("city");
+  let city_name = city.options[city.selectedIndex].text;
+  console.log("city_name:", city_name);
+  let open_work = document.getElementById("open_work").value;
+  console.log("open_work:", open_work);
+  let min_sal = document.getElementById("min_sal").value;
+  console.log("min_sal:", min_sal);
+  let join_dur = document.getElementById("duration");
+  let joining_duration = join_dur.options[join_dur.selectedIndex].text;
+  console.log("joining_duration:", joining_duration);
+  let int_comp_name = document.getElementById("int_comp_name").value;
+  console.log("int_comp_name:", int_comp_name);
+  let degree = document.getElementById("degree");
+  let degree_name = degree.options[degree.selectedIndex].text;
+  console.log("degree_name:", degree_name);
+  let speci = document.getElementById("course_spe");
+  let course_specia = speci.options[speci.selectedIndex].text;
+  console.log("course_specia:", course_specia);
+  let year = document.getElementById("passing_year");
+  let passing_year = year.options[year.selectedIndex].text;
+  console.log("passing_year:", passing_year);
+  let uni_namme = document.getElementById("uni_name").value;
+  console.log("uni_namme:", uni_namme);
+  let resu = document.getElementById("resume").value;
+  console.log("resu:", resu);
 }
