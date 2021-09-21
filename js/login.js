81f3dbf0c2ca6d4b1636e52fb592a158c032a812
Shubham Thakur <shubhamthakur2920@gmail.com>
2021-09-21 16:42:11 +0530

    login page authentication

--- js/login.js
+++ js/login.js
@@ -12,25 +12,31 @@ function login_user(e) {
   let users = JSON.parse(localStorage.getItem("All_Valid_Users"));
   let userExist = false;
   users.forEach((element) => {
-    if (element.email === login_email && element.password === login_password) {
-      userExist = true;
-      login_email = "";
-      login_password = "";
-      localStorage.setItem(
-        "userLogged",
-        JSON.stringify([{ isLogged: true }, element])
-      );
+    if (element.email === login_email) {
+      if (element.password === login_password) {
+        userExist = true;
+        login_email = "";
+        login_password = "";
+        localStorage.setItem(
+          "userLogged",
+          JSON.stringify([{ isLogged: true }, element])
+        );
 
-      alert(`Login Successfully`);
-      location.href = "../assets/opportunities.html";
-      document.getElementById("login_email").value = "";
-      document.getElementById("login_password").value = "";
-      return;
+        alert(`Login Successfully`);
+        location.href = "../assets/opportunities.html";
+        document.getElementById("login_email").value = "";
+        document.getElementById("login_password").value = "";
+        return;
+      } else {
+        alert(`Invalid Password`);
+        document.getElementById("login_password").value = "";
+        userExist = true;
+      }
     }
   });
 
   if (userExist === false) {
-    alert(`Invalid Credentials`);
+    alert(`User doesn't Exist`);
     document.getElementById("login_email").value = "";
     document.getElementById("login_password").value = "";
   }
